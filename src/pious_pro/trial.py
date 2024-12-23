8adad650fe6f415ecbb8969b17b26879cb8da07f
bkushigian <bkushigian@gmail.com>
2024-12-23 12:37:28 -0800

    Removed trial period for now

--- src/pious_pro/trial.py
+++ src/pious_pro/trial.py
@@ -1,6 +1,7 @@
 import datetime
 
 trial_end = datetime.date(2024, 12, 31)
-if datetime.date.today() > trial_end:
+# if datetime.date.today() > trial_end:
+if False:
     print("Trial expired. Please contact software maintainer.")
     exit(2)
